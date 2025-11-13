a1eb37e4fe9acd13840c6c04cfa651cf4bf9430f
udituen <udituen@gmail.com>
2025-11-13 18:36:02 -0500

    add asset folder

---
+++ app/main.py
@@ -0,0 +1,11 @@
+"""
+This file contains the main app logic.
+we will be using Autotenizer, automodelforcausallm
+
+"""
+
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+
+
